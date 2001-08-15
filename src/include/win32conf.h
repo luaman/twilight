e135b5e4715f5eace01f6492f4ee86c260b66b04
digiman <digiman@d7cf8633-e32d-0410-b094-e92efae38249>
2001-08-15 23:54:31 +0000

    Now valid macro here.

--- src/include/win32conf.h
+++ src/include/win32conf.h
@@ -6,6 +6,10 @@
 #ifndef __WIN32CONF_H
 #define __WIN32CONF_H
 
+#if _MSC_VER >= 800	/* MSVC 4.0 */
+#pragma warning( disable : 4244 4127 4201 4214 4514 4305 4115 4018)
+#endif
+
 #define VERSION			"0.0.0"
 
 #define USERPATH		"."
