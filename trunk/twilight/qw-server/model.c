bba7b9a0e0ae650868674c2c749a98f8e92f9155
raineth <raineth@d7cf8633-e32d-0410-b094-e92efae38249>
2001-09-29 22:45:12 +0000

    This isn't the commit you're looking for.  We can go about our business.  Move along.

--- trunk/twilight/qw-server/model.c
+++ trunk/twilight/qw-server/model.c
@@ -1062,13 +1062,13 @@ Mod_LoadBrushModel (model_t *mod, void *buffer)
 		if (i == LUMP_ENTITIES)
 			continue;
 		mod->checksum ^= Com_BlockChecksum (mod_base + 
-			header->lumps[i].fileofs,  header->lumps[i].filelen));
+			header->lumps[i].fileofs,  header->lumps[i].filelen);
 
 		if (i == LUMP_VISIBILITY || i == LUMP_LEAFS || i == LUMP_NODES)
 			continue;
 
 		mod->checksum2 ^= Com_BlockChecksum (mod_base +
-			header->lumps[i].fileofs, header->lumps[i].filelen));
+			header->lumps[i].fileofs, header->lumps[i].filelen);
 	}
 
 	Mod_LoadVertexes (&header->lumps[LUMP_VERTEXES]);
