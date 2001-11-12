79945461bf0f20fa9c5eda8eb7846eb7910aab5a
knghtbrd <knghtbrd@d7cf8633-e32d-0410-b094-e92efae38249>
2001-11-12 21:19:52 +0000

    Mercury wanted this.  =)

---
+++ trunk/twilight/src/renderer/noise.c
@@ -0,0 +1,110 @@
+/*
+	$RCSfile$
+
+	Copyright (C) 2000  Forest Hale
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+
+	See the GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to:
+	
+		Free Software Foundation, Inc.
+		59 Temple Place - Suite 330
+		Boston, MA  02111-1307, USA
+*/
+static const char rcsid[] =
+	"$Id$";
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#else
+# ifdef _WIN32
+#  include <win32conf.h>
+# endif
+#endif
+
+#include "SDL_types.h"
+#include "qtypes.h"
+#include "mathlib.h"
+#include "strlib.h"
+#include "sys.h"
+#include "zone.h"
+
+void
+FractalNoise (Uint8 *noise, int size, int startgrid)
+{
+	int x, y, g, g2;
+	int amplitude, min, max;
+	int size1 = size - 1;
+	int sizepower, gridpower;
+	int *noisebuf;
+#define n(x,y) noisebuf[((y)&size1)*size+((x)&size1)]
+
+	for (sizepower = 0;(1 << sizepower) < size;sizepower++);
+	if (size != (1 << sizepower))
+		Sys_Error("FractalNoise: size must be power of 2\n");
+
+	for (gridpower = 0;(1 << gridpower) < startgrid;gridpower++);
+	if (startgrid != (1 << gridpower))
+		Sys_Error("FractalNoise: grid must be power of 2\n");
+
+	startgrid = bound(0, startgrid, size);
+
+	amplitude = 0xFFFF; // this gets halved before use
+	noisebuf = Z_Malloc(size*size*sizeof(int));
+
+	for (g2 = startgrid;g2;g2 >>= 1)
+	{
+		// brownian motion (at every smaller level there is random behavior)
+		amplitude >>= 1;
+		for (y = 0;y < size;y += g2)
+			for (x = 0;x < size;x += g2)
+				n(x,y) += (rand()&amplitude);
+
+		g = g2 >> 1;
+		if (g)
+		{
+			// subdivide, diamond-square algorythm
+			// diamond
+			for (y = 0;y < size;y += g2)
+				for (x = 0;x < size;x += g2)
+					n(x+g,y+g) = (n(x,y) + n(x+g2,y) + n(x,y+g2)
+							+ n(x+g2,y+g2)) >> 2;
+			// square
+			for (y = 0;y < size;y += g2)
+				for (x = 0;x < size;x += g2)
+				{
+					n(x+g,y) = (n(x,y) + n(x+g2,y) + n(x+g,y-g)
+							+ n(x+g,y+g)) >> 2;
+					n(x,y+g) = (n(x,y) + n(x,y+g2) + n(x-g,y+g)
+							+ n(x+g,y+g)) >> 2;
+				}
+		}
+	}
+	// find range of noise values
+	min = max = 0;
+	for (y = 0;y < size;y++)
+		for (x = 0;x < size;x++)
+		{
+			if (n(x,y) < min) min = n(x,y);
+			if (n(x,y) > max) max = n(x,y);
+		}
+	max -= min;
+	max++;
+	// normalize noise and copy to output
+	for (y = 0;y < size;y++)
+		for (x = 0;x < size;x++)
+			*noise++ = (Uint8) (((n(x,y) - min) * 256) / max);
+	Z_Free (noisebuf);
+#undef n
+}
+
