f193bd681340e39630394c2625054cc2b8d65cb0
eviltypeguy <eviltypeguy@d7cf8633-e32d-0410-b094-e92efae38249>
2001-11-24 23:16:15 +0000

    Added our standard header, tacked on a copyright for LordHavoc, and added the necessary includes since string library functions are being used...

--- src/qw/gl_poly.c
+++ src/qw/gl_poly.c
@@ -1,7 +1,43 @@
+/*
+	$RCSfile$ -- gl_poly.c
+
+	Copyright (C) 2001 Forest "LordHavoc" Hale
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+
+	See the GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to:
+	
+		Free Software Foundation, Inc.
+		59 Temple Place - Suite 330
+		Boston, MA  02111-1307, USA
+
+*/
+static const char rcsid[] =
+    "$Id$";
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#else
+# ifdef _WIN32
+#  include <win32conf.h>
+# endif
+#endif
+
 #include "quakedef.h"
 #include "mathlib.h"
 #include "common.h"
 #include "glquake.h"
+#include "strlib.h"
 
 transvert_t transvert[MAX_TRANSVERTS];
 transpoly_t transpoly[MAX_TRANSPOLYS];
