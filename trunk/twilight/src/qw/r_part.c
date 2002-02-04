7ab42cc391a2abf1aa2aeffd4b37c79b8e563e78
eviltypeguy <eviltypeguy@d7cf8633-e32d-0410-b094-e92efae38249>
2002-02-04 02:55:28 +0000

    superflous formatting changes, brought to you by Because I Should(TM)

--- trunk/twilight/src/qw/r_part.c
+++ trunk/twilight/src/qw/r_part.c
@@ -72,17 +72,17 @@ typedef enum {
 } ptype_t;
 
 typedef struct {
-	vec3_t		org1;
-	vec3_t		org2;
+	vec3_t	org1;
+	vec3_t	org2;
 
-	vec3_t		normal;
-	vec4_t		color;
-	float		scale1;
-	float		scale2;
-	float		ramp;
-	float		die;
-	float		rstep;
-	ptype_t		type;
+	vec3_t	normal;
+	vec4_t	color;
+	float	scale1;
+	float	scale2;
+	float	ramp;
+	float	die;
+	float	rstep;
+	ptype_t	type;
 } tube_particle_t;
 
 static tube_particle_t *tube_particles, **free_tube_particles;
@@ -92,13 +92,11 @@ inline qboolean
 new_tube_particle (ptype_t type, vec3_t org1, vec3_t org2, vec4_t color,
 		float ramp, float rstep, float scale1, float scale2, float die)
 {
-	tube_particle_t	   *p;
-	vec3_t				normal;
+	tube_particle_t	*p;
+	vec3_t			normal;
 
-	if (num_tube_particles >= max_tube_particles) {
-		// Out of particles.
+	if (num_tube_particles >= max_tube_particles)	// Out of particles.
 		return false;
-	}
 
 	p = &tube_particles[num_tube_particles++];
 	p->type = type;
@@ -120,17 +118,17 @@ new_tube_particle (ptype_t type, vec3_t org1, vec3_t org2, vec4_t color,
 	
 typedef struct {
 	// Some effects need a base origin.
-	vec3_t		org1;
-	vec3_t		org2;
-	vec3_t		org3;
+	vec3_t	org1;
+	vec3_t	org2;
+	vec3_t	org3;
 
-	vec3_t		normal;
-	vec4_t		color1;
-	vec4_t		color2;
-	float		scale;
-	float		ramp;
-	float		die;
-	ptype_t		type;
+	vec3_t	normal;
+	vec4_t	color1;
+	vec4_t	color2;
+	float	scale;
+	float	ramp;
+	float	die;
+	ptype_t	type;
 } cone_particle_t;
 
 static cone_particle_t *cone_particles, **free_cone_particles;
@@ -143,10 +141,8 @@ new_cone_particle (ptype_t type, vec3_t org1, vec3_t org2, vec3_t org3,
 	cone_particle_t	   *p;
 	vec3_t				normal;
 
-	if (num_cone_particles >= max_cone_particles) {
-		// Out of particles.
+	if (num_cone_particles >= max_cone_particles)	// Out of particles.
 		return false;
-	}
 
 	p = &cone_particles[num_cone_particles++];
 	p->type = type;
@@ -167,20 +163,20 @@ new_cone_particle (ptype_t type, vec3_t org1, vec3_t org2, vec3_t org3,
 }
 	
 typedef struct {
-	ptype_t		type;
-	vec3_t		org;
-	vec3_t		oldorg;		// used to store last position to traceline can be used
+	ptype_t	type;
+	vec3_t	org;
+	vec3_t	oldorg;		// used to store last position to traceline can be used
 							// to allow particles to have bounce physics, etc.
-	vec3_t		vel;
-	vec4_t		color;
-	float		bounce;		// how much bounce-back from a surface the particle hits
+	vec3_t	vel;
+	vec4_t	color;
+	float	bounce;		// how much bounce-back from a surface the particle hits
 							// (0 = no physics, 1 = stop and slide, 2 = keep bouncing forever,
 							// 1.5 is typical)
-	float		die;
-	float		friction;	// how much air friction affects this object (objects with a low
+	float	die;
+	float	friction;	// how much air friction affects this object (objects with a low
 							// mass/size ratio tend to get more air friction)
-	float		ramp;
-	float		scale;
+	float	ramp;
+	float	scale;
 } base_particle_t;
 
 static base_particle_t *base_particles, **free_base_particles;
@@ -190,10 +186,9 @@ inline qboolean
 new_base_particle (ptype_t type, vec3_t org, vec3_t vel, vec4_t color,
 		float ramp, float scale, float die, float bounce, float friction)
 {
-	base_particle_t	   *p;
+	base_particle_t	*p;
 
-	if (num_base_particles >= max_base_particles)
-		// Out of particles.
+	if (num_base_particles >= max_base_particles) // Out of particles.
 		return false;
 
 	p = &base_particles[num_base_particles++];
@@ -214,7 +209,7 @@ inline qboolean
 new_base_particle_oc (ptype_t type, vec3_t org, vec3_t vel, int color,
 		float ramp, float scale, float die, float bounce, float friction)
 {
-	vec4_t		vcolor;
+	vec4_t	vcolor;
 
 	VectorCopy4 (d_8tofloattable[color], vcolor);	
 	return new_base_particle (type, org, vel, vcolor, ramp, scale, die,
@@ -229,7 +224,7 @@ R_InitParticles
 void
 R_InitParticles (void)
 {
-	int			i;
+	int	i;
 
 	r_particles = Cvar_Get ("r_particles", "1", CVAR_NONE, NULL);
 	r_base_particles = Cvar_Get ("r_base_particles", "1", CVAR_NONE, NULL);
@@ -240,11 +235,10 @@ R_InitParticles (void)
 
 	i = COM_CheckParm ("-particles");
 
-	if (i) {
+	if (i)
 		max_base_particles = (int) (Q_atoi (com_argv[i + 1]));
-	} else {
+	else
 		max_base_particles = MAX_PARTICLES;
-	}
 
 	max_base_particles = max(max_base_particles, ABSOLUTE_MIN_PARTICLES);
 	max_cone_particles = max_base_particles;
@@ -278,17 +272,15 @@ float beamlength = 16;
 void
 R_EntityParticles (entity_t *ent)
 {
-	int			i;
-	float		angle;
-	float		sr, sp, sy, cr, cp, cy;
-	vec3_t		forward, org;
-	float		dist = 64;
+	int		i;
+	float	angle, dist, sr, sp, sy, cr, cp, cy;
+	vec3_t	forward, org;
 
-	if (!avelocities[0][0]) {
+	if (!avelocities[0][0])
 		for (i = 0; i < NUMVERTEXNORMALS * 3; i++)
 			avelocities[0][i] = (Q_rand () & 255) * 0.01;
-	}
 
+	dist = 64;
 	for (i = 0; i < NUMVERTEXNORMALS; i++) {
 		angle = cl.time * avelocities[i][0];
 		sy = Q_sin (angle);
@@ -314,7 +306,6 @@ R_EntityParticles (entity_t *ent)
 	}
 }
 
-
 /*
 ===============
 R_ClearParticles
@@ -328,16 +319,14 @@ R_ClearParticles (void)
 	num_tube_particles = 0;
 }
 
-
 void
 R_ReadPointFile_f (void)
 {
-	FILE			   *f;
-	vec3_t				org;
-	int					r;
-	int					c;
-	char				name[MAX_OSPATH];
-	extern cvar_t	   *cl_mapname;
+	extern cvar_t	*cl_mapname;
+	FILE			*f;
+	vec3_t			org;
+	int				r, c;
+	char			name[MAX_OSPATH];
 
 	snprintf (name, sizeof (name), "maps/%s.pts", cl_mapname->string);
 
@@ -353,8 +342,8 @@ R_ReadPointFile_f (void)
 		r = fscanf (f, "%f %f %f\n", &org[0], &org[1], &org[2]);
 		if (r != 3)
 			break;
-		c++;
 
+		c++;
 		new_base_particle_oc (pt_static, org, r_origin, (-c) & 15, 0, -1,
 			99999, 0, 0);
 	}
@@ -373,8 +362,8 @@ Parse an effect out of the server message
 void
 R_ParseParticleEffect (void)
 {
-	vec3_t		org, dir;
-	int			i, count, msgcount, color;
+	vec3_t	org, dir;
+	int		i, count, msgcount, color;
 
 	for (i = 0; i < 3; i++)
 		org[i] = MSG_ReadCoord ();
@@ -390,14 +379,13 @@ R_ParseParticleEffect (void)
 /*
 ===============
 R_ParticleExplosion
-
 ===============
 */
 void
 R_ParticleExplosion (vec3_t org)
 {
-	int			i, j, type;
-	vec3_t		porg, vel;
+	int		i, j, type;
+	vec3_t	porg, vel;
 
 	for (i = 0; i < 512; i++) {
 		type = (i & 1) ? pt_explode : pt_explode2;
@@ -419,10 +407,10 @@ R_ParticleExplosion2
 void
 R_ParticleExplosion2 (vec3_t org, int colorStart, int colorLength)
 {
-	int			i, j;
-	int			colorMod = 0, color;
-	vec3_t		porg, vel;
+	int		i, j, colorMod, color;
+	vec3_t	porg, vel;
 
+	colorMod = 0;
 	for (i = 0; i < 512; i++) {
 		color = colorStart + (colorMod % colorLength);
 		colorMod++;
@@ -444,10 +432,10 @@ R_BlobExplosion
 void
 R_BlobExplosion (vec3_t org)
 {
-	int			i, j, color;
-	float		pdie;
-	vec3_t		porg, pvel;
-	ptype_t		ptype;
+	int		i, j, color;
+	float	pdie;
+	vec3_t	porg, pvel;
+	ptype_t	ptype;
 
 	for (i = 0; i < 1024; i++) {
 		pdie = 1 + (Q_rand () & 8) * 0.05;
@@ -477,9 +465,9 @@ R_RunParticleEffect
 void
 R_RunParticleEffect (vec3_t org, vec3_t dir, int color, int count)
 {
-	int			i, j, pcolor;
-	float		pdie;
-	vec3_t		porg, pvel;
+	int		i, j, pcolor;
+	float	pdie;
+	vec3_t	porg, pvel;
 
 	for (i = 0; i < count; i++) {
 		pdie = 0.1 * (Q_rand () % 5);
@@ -503,9 +491,9 @@ R_LavaSplash
 void
 R_LavaSplash (vec3_t org)
 {
-	int			i, j, pcolor;
-	float		vel, pdie;
-	vec3_t		dir, porg, pvel;
+	int		i, j, pcolor;
+	float	vel, pdie;
+	vec3_t	dir, porg, pvel;
 
 	for (i = -16; i < 16; i++)
 		for (j = -16; j < 16; j++) {
@@ -537,8 +525,8 @@ R_TeleportSplash
 void
 R_TeleportSplash (vec3_t org)
 {
-	vec3_t		porg_top, porg_bottom;
-	vec4_t		color1, color2;
+	vec3_t	porg_top, porg_bottom;
+	vec4_t	color1, color2;
 
 	org[2] += 4;
 
@@ -568,8 +556,8 @@ R_Torch
 void 
 R_Torch (entity_t *ent, qboolean torch2)
 {
-	vec3_t		porg, pvel;
-	vec4_t		color;
+	vec3_t	porg, pvel;
+	vec4_t	color;
 
 	if (!r_particles->value)
 		return;
@@ -586,11 +574,9 @@ R_Torch (entity_t *ent, qboolean torch2)
 		VectorSet (pvel, (Q_rand() & 7) - 4, (Q_rand() & 7) - 4, 0);
 		new_base_particle (pt_torch2, porg, pvel, color, Q_rand () & 3,
 			ent->cur.frame ? 30 : 10, 5, 0, 0);
-	} else { 
-		// wall torches
+	} else	// wall torches
 		new_base_particle (pt_torch, porg, pvel, color, Q_rand () & 3,
 			10, 5, 0, 0);
-	}
 }
 
 void
@@ -598,8 +584,7 @@ R_RailTrail (vec3_t start, vec3_t end)
 {
 	vec3_t	vec, org, vel, right;
 	vec4_t	color;
-	float	len, roll = 0.0f;
-	float	sr, sp, sy, cr, cp, cy;
+	float	sr, sp, sy, cr, cp, cy, len, roll = 0.0f;
 
 	VectorSubtract (end, start, vec);
 	Vector2Angles (vec, org);
@@ -647,9 +632,9 @@ R_RailTrail (vec3_t start, vec3_t end)
 void
 R_RocketTubeTrail (vec3_t start, vec3_t end, int type)
 {
-	vec3_t		vec;
-	vec4_t		color;
-	float		len;
+	vec3_t	vec;
+	vec4_t	color;
+	float	len;
 
 	VectorSubtract (end, start, vec);
 	len = VectorLength (vec);
@@ -663,16 +648,13 @@ R_RocketTubeTrail (vec3_t start, vec3_t end, int type)
 }
 
 void
-R_RocketTrail (vec3_t in_start, vec3_t in_end, int type)
+R_RocketTrail (vec3_t start, vec3_t end, int type)
 {
-	vec3_t		vec, avec, porg, pvel, start, end;
+	vec3_t		vec, avec, porg, pvel;
 	float		len, pdie, pramp;
 	int			j, lsub, pcolor;
 	static int	tracercount;
 	ptype_t		ptype;
-
-	VectorCopy (in_start, start);
-	VectorCopy (in_end, end);
 
 	if (type == 0) {
 		R_RocketTubeTrail (start, end, type);
@@ -694,7 +676,7 @@ R_RocketTrail (vec3_t in_start, vec3_t in_end, int type)
 		ptype = 0;
 
 		switch (type) {
-			case 1:					// smoke smoke
+			case 1:					// smoke
 				pramp = (Q_rand () & 3) + 2;
 				pcolor = ramp3[(int) pramp];
 				ptype = pt_fire;
@@ -761,12 +743,10 @@ R_Move_Base_Particles (void)
 #ifdef MOD_POINTINLEAF
 	mleaf_t				*mleaf;
 #endif
-	base_particle_t	   *p;
-	int					i, j, k, activeparticles, maxparticle;
-	float				time1, time2, time3;
-	float				grav, dvel;
-	float				frametime;
-	vec3_t				v;
+	base_particle_t	*p;
+	int				i, j, k, activeparticles, maxparticle;
+	float			time1, time2, time3, grav, dvel, frametime;
+	vec3_t			v;
 
 	qglBindTexture (GL_TEXTURE_2D, part_tex_dot);
 
@@ -932,9 +912,9 @@ R_Draw_Base_Particles
 static void
 R_Draw_Base_Particles (void)
 {
-	base_particle_t	   *p;
-	int					j, i, activeparticles, maxparticle;
-	float				scale, *corner;
+	base_particle_t	*p;
+	int				j, i, activeparticles, maxparticle;
+	float			scale, *corner;
 
 	qglBindTexture (GL_TEXTURE_2D, part_tex_dot);
 
@@ -949,6 +929,9 @@ R_Draw_Base_Particles (void)
 			continue;
 		}
 
+		if (r_particle_physics->value)
+			VectorCopy(p->org, p->oldorg);
+
 		maxparticle = i;
 		activeparticles++;
 
@@ -960,9 +943,8 @@ R_Draw_Base_Particles (void)
 				scale = -p->scale;
 			else
 				scale = (-p->scale) + scale * 0.004;
-		} else {
+		} else
 			scale = p->scale;
-		}
 
 		VectorCopy4 (p->color, c_array[v_index + 0]);
 		VectorCopy4 (p->color, c_array[v_index + 1]);
@@ -1017,9 +999,9 @@ R_Move_Tube_Particles
 static void
 R_Move_Tube_Particles (void)
 {
-	tube_particle_t	   *p;
-	int					i, j, activeparticles, maxparticle;
-	float				frametime;
+	tube_particle_t	*p;
+	int				i, j, activeparticles, maxparticle;
+	float			frametime;
 
 	frametime = cl.time - cl.oldtime;
 
@@ -1045,7 +1027,6 @@ R_Move_Tube_Particles (void)
 				p->color[3] -= frametime * 0.5;
 				if (p->color[3] <= 0)
 					p->die = -1;
-//				p->color[0] -= frametime * 0.5;
 				if (p->color[1] < p->color[0]) {
 					p->color[1] += frametime * 0.40;
 					p->color[2] += frametime * 0.40;
@@ -1077,12 +1058,11 @@ R_Draw_Tube_Particles
 static void
 R_Draw_Tube_Particles (void)
 {
-	tube_particle_t	   *p;
-	int					i, j, k, activeparticles, maxparticle;
-	int					v0, v1, v2, v3, v_f0, v_f1;
-	float				f;
-	vec3_t				v_up, v_right;
-	float			   *bub_sin, *bub_cos;
+	tube_particle_t	*p;
+	int				i, j, k, activeparticles, maxparticle;
+	int				v0, v1, v2, v3, v_f0, v_f1;
+	float			f, *bub_sin, *bub_cos;
+	vec3_t			v_up, v_right;
 
 	qglBindTexture (GL_TEXTURE_2D, part_tex_smoke);
 	if (gl_cull->value)
@@ -1119,10 +1099,6 @@ R_Draw_Tube_Particles (void)
 			VectorCopy4 (p->color, c_array[v_index]);
 			VectorSet2 (tc_array[v_index], f/16.0f, 
 					DotProduct(p->org1, p->normal) / 64);
-			/*
-			VectorSet2 (tc_array[v_index], (bub_cos[j] * 0.5) + 0.5,
-					(bub_sin[i] * 0.5) + 0.5);
-					*/
 			v2 = v_index++;
 
 			VectorTwiddle (p->org2, v_right, bub_cos[i%16], v_up, bub_sin[i%16],
@@ -1130,11 +1106,6 @@ R_Draw_Tube_Particles (void)
 			VectorCopy4 (p->color, c_array[v_index]);
 			VectorSet2 (tc_array[v_index], f/16.0f, 
 					DotProduct(p->org2, p->normal) / 64);
-//			VectorSet2 (tc_array[v_index], i/16.0f, 1);
-			/*
-			VectorSet2 (tc_array[v_index], (bub_cos[j] * 0.5) - 0.5,
-					(bub_sin[i] * 0.5) - 0.5);
-					*/
 			v3 = v_index++;
 
 			if (v_f0 == v_f1) {
@@ -1188,9 +1159,9 @@ R_Move_Cone_Particles
 static void
 R_Move_Cone_Particles (void)
 {
-	cone_particle_t	   *p;
-	int					j, k, activeparticles, maxparticle;
-	float				frametime, teletime;
+	cone_particle_t	*p;
+	int				j, k, activeparticles, maxparticle;
+	float			frametime, teletime;
 
 	frametime = cl.time - cl.oldtime;
 	teletime = frametime * 120;
@@ -1251,7 +1222,6 @@ R_Move_Cone_Particles (void)
 				p->color1[3] -= frametime * 0.5;
 				if (p->color1[3] <= 0)
 					p->die = -1;
-//				p->color1[0] -= frametime * 0.5;
 				if (p->color1[1] < p->color1[0]) {
 					p->color1[1] += frametime * 0.50;
 					p->color1[2] += frametime * 0.50;
@@ -1282,11 +1252,11 @@ R_Draw_Cone_Particles
 static void
 R_Draw_Cone_Particles (void)
 {
-	cone_particle_t	   *p;
-	int					i, i2, j, k, activeparticles, maxparticle;
-	int					v_center, v_first, v_last;
-	vec3_t				v_up, v_right;
-	float			   *bub_sin, *bub_cos;
+	cone_particle_t	*p;
+	int				i, i2, j, k, activeparticles, maxparticle;
+	int				v_center, v_first, v_last;
+	vec3_t			v_up, v_right;
+	float			*bub_sin, *bub_cos;
 
 	qglBindTexture (GL_TEXTURE_2D, part_tex_smoke_ring);
 	if (gl_cull->value)
