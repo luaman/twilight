5ed5e986a60fcee0e54fc9b33ec200a162e4a12e
knghtbrd <knghtbrd@d7cf8633-e32d-0410-b094-e92efae38249>
2002-04-07 08:01:17 +0000

    LordHavoc's RotatePointAroundVector, it's faster and it doesn't have to disable the optimizer for MSVC.

--- src/base/mathlib.c
+++ src/base/mathlib.c
@@ -446,79 +446,39 @@ void VectorVectors(const vec3_t forward, vec3_t right, vec3_t up)
 	CrossProduct(right, forward, up);
 }
 
-#if defined(__WIN32) && _MSC_VER >= 800	/* MSVC 4.0 */
-#pragma optimize( "", off )
-#endif
-
-
 void
 RotatePointAroundVector (vec3_t dst, const vec3_t dir, const vec3_t point,
-						 float degrees)
+		float degrees)
 {
-	float       m[3][3];
-	float       im[3][3];
-	float       zrot[3][3];
-	float       tmpmat[3][3];
-	float       rot[3][3];
-	int         i;
-	vec3_t      vr, vup, vf;
+	float		t0, t1;
+	float		angle, c, s;
+	vec3_t		vr, vu, vf;
 
-	vf[0] = dir[0];
-	vf[1] = dir[1];
-	vf[2] = dir[2];
+	angle = DEG2RAD (degrees);
 
-	PerpendicularVector (vr, dir);
-	CrossProduct (vr, vf, vup);
+	c = cos (angle);
+	s = sin (angle);
 
-	m[0][0] = vr[0];
-	m[1][0] = vr[1];
-	m[2][0] = vr[2];
+	VectorCopy (dir, vf);
+	VectorVectors (vf, vr, vu);
 
-	m[0][1] = vup[0];
-	m[1][1] = vup[1];
-	m[2][1] = vup[2];
+	t0 = vr[0] *  c + vu[0] * -s;
+	t1 = vr[0] *  s + vu[0] *  c;
+	dst[0] = (t0 * vr[0] + t1 * vu[0] + vf[0] * vf[0]) * point[0]
+		+ (t0 * vr[1] + t1 * vu[1] + vf[0] * vf[1]) * point[1]
+		+ (t0 * vr[2] + t1 * vu[2] + vf[0] * vf[2]) * point[2];
 
-	m[0][2] = vf[0];
-	m[1][2] = vf[1];
-	m[2][2] = vf[2];
+	t0 = vr[1] *  c + vu[1] * -s;
+	t1 = vr[1] *  s + vu[1] *  c;
+	dst[1] = (t0 * vr[0] + t1 * vu[0] + vf[1] * vf[0]) * point[0]
+		+ (t0 * vr[1] + t1 * vu[1] + vf[1] * vf[1]) * point[1]
+		+ (t0 * vr[2] + t1 * vu[2] + vf[1] * vf[2]) * point[2];
 
-	memcpy (im, m, sizeof (im));
-
-	im[0][1] = m[1][0];
-	im[0][2] = m[2][0];
-	im[1][0] = m[0][1];
-	im[1][2] = m[2][1];
-	im[2][0] = m[0][2];
-	im[2][1] = m[1][2];
-
-	memset (zrot, 0, sizeof (zrot));
-	zrot[0][0] = zrot[1][1] = zrot[2][2] = 1.0F;
-
-	zrot[0][0] = Q_cos (DEG2RAD (degrees));
-	zrot[0][1] = Q_sin (DEG2RAD (degrees));
-	zrot[1][0] = -zrot[0][1];
-	zrot[1][1] = zrot[0][0];
-
-	R_ConcatRotations (m, zrot, tmpmat);
-	R_ConcatRotations (tmpmat, im, rot);
-
-	for (i = 0; i < 3; i++) {
-		dst[i] =
-			rot[i][0] * point[0] + rot[i][1] * point[1] + rot[i][2] * point[2];
-	}
-}
-
-#if defined(__WIN32) && _MSC_VER >= 800	/* MSVC 4.0 */
-#pragma optimize( "", on )
-#endif
-
-/*-----------------------------------------------------------------*/
-
-float
-anglemod (float a)
-{
-	a = (360.0 / 65536) * ((int) (a * (65536 / 360.0)) & 65535);
-	return a;
+	t0 = vr[2] *  c + vu[2] * -s;
+	t1 = vr[2] *  s + vu[2] *  c;
+	dst[2] = (t0 * vr[0] + t1 * vu[0] + vf[2] * vf[0]) * point[0]
+		+ (t0 * vr[1] + t1 * vu[1] + vf[2] * vf[1]) * point[1]
+		+ (t0 * vr[2] + t1 * vu[2] + vf[2] * vf[2]) * point[2];
 }
 
 
