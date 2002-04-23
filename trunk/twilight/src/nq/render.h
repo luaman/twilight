5f67fcf703ecead3d371bf5796eabf08e295a49d
knghtbrd <knghtbrd@d7cf8633-e32d-0410-b094-e92efae38249>
2002-04-23 04:15:11 +0000

    render.h is a little more merged than it was.  Not necessarily any cleaner, but not noticably much worse either.  Just less disparity now.

--- trunk/twilight/src/nq/render.h
+++ trunk/twilight/src/nq/render.h
@@ -23,7 +23,6 @@
 
 	$Id$
 */
-// refresh.h -- public interface to refresh functions
 
 #ifndef __RENDER_H
 #define __RENDER_H
@@ -33,39 +32,47 @@
 #include "model.h"
 
 #define	MAXCLIPPLANES	11
-
 #define	TOP_RANGE		16				// soldier uniform colors
 #define	BOTTOM_RANGE	96
 
 //=============================================================================
 
 typedef struct colormap_s {
-	vec3_t	top;
-	vec3_t	bottom;
+	vec3_t		top;
+	vec3_t		bottom;
 } colormap_t;
 
 typedef struct entity_s {
-	qboolean    forcelink;				// model changed
+	// model changed
+	qboolean		forcelink;
 
-	entity_state_t baseline;			// to fill in defaults in updates
+	// to fill in defaults in updates
+	entity_state_t	baseline;
 
-	double      msgtime;				// time of last update
-	vec3_t      msg_origins[2];			// last two updates (0 is newest) 
-	vec3_t      origin;
-	vec3_t      msg_angles[2];			// last two updates (0 is newest)
-	vec3_t      angles;
-	struct model_s *model;				// NULL = no model
-	int         frame;
-	float       syncbase;				// for client-side animations
-	colormap_t	*colormap;
-	int         effects;				// light, particals, etc
-	int         skinnum;				// for Alias models
-	int         visframe;				// last frame this entity was
-	// found in an active leaf
+	// time of last update
+	double			msgtime;
+	// last two updates (0 is newest) 
+	vec3_t			msg_origins[2];
+	vec3_t			origin;
+	// last two updates (0 is newest)
+	vec3_t			msg_angles[2];
+	vec3_t			angles;
+	// NULL = no model
+	struct model_s	*model;
+	int				frame;
+	// for client-side animations
+	float			syncbase;
+	colormap_t		*colormap;
+	// light, particals, etc
+	int				effects;
+	// for Alias models
+	int				skinnum;
+	// last frame this entity was found in an active leaf
+	int				visframe;
 
-	vec3_t		last_light;
-	float		time_left;
-	struct model_s *lastmodel;
+	vec3_t			last_light;
+	float			time_left;
+	struct model_s	*lastmodel;
 } entity_t;
 
 typedef struct {
@@ -84,77 +91,68 @@ typedef struct {
 //
 // refresh
 //
-extern int  reinit_surfcache;
-
-
 extern refdef_t r_refdef;
 extern vec3_t r_origin, vpn, vright, vup;
 
 extern struct texture_s *r_notexture_mip;
 
 
-void        R_Init_Cvars (void);
-void        R_Init (void);
-void        R_InitTextures (void);
-void        R_RenderView (void);		// must set r_refdef first
-void        R_ViewChanged (vrect_t *pvrect, int lineadj, float aspect);
+void R_Init_Cvars (void);
+void R_Init (void);
+void R_InitTextures (void);
 
-								// called whenever r_refdef or vid change
-void        R_InitSky (struct texture_s *mt);	// called at level load
+// must set r_refdef first
+void R_RenderView (void);
 
-void        R_NewMap (void);
+// called whenever r_refdef or vid change
+void R_ViewChanged (vrect_t *pvrect, int lineadj, float aspect);
+
+// called at level load
+void R_InitSky (struct texture_s *mt);
+
+void R_NewMap (void);
 
 
-void        R_ParseParticleEffect (void);
-void        R_RunParticleEffect (vec3_t org, vec3_t dir, int color, int count);
-void        R_RocketTrail (vec3_t start, vec3_t end);
-void        R_ParticleTrail (vec3_t start, vec3_t end, int type);
+void R_ParseParticleEffect (void);
+void R_RunParticleEffect (vec3_t org, vec3_t dir, int color, int count);
+void R_RocketTrail (vec3_t start, vec3_t end);
+void R_ParticleTrail (vec3_t start, vec3_t end, int type);
 
-void        R_EntityParticles (entity_t *ent);
-void        R_BlobExplosion (vec3_t org);
-void        R_ParticleExplosion (vec3_t org);
-void        R_ParticleExplosion2 (vec3_t org, int colorStart, int colorLength);
-void        R_LavaSplash (vec3_t org);
-void        R_TeleportSplash (vec3_t org);
-void		R_RailTrail (vec3_t start, vec3_t end);
+void R_EntityParticles (entity_t *ent);
+void R_BlobExplosion (vec3_t org);
+void R_ParticleExplosion (vec3_t org);
+void R_ParticleExplosion2 (vec3_t org, int colorStart, int colorLength);
+void R_LavaSplash (vec3_t org);
+void R_TeleportSplash (vec3_t org);
+void R_RailTrail (vec3_t start, vec3_t end);
 
+//
+// gl_rlight.c
+//
 #define	MAX_DLIGHTS		32
 
 typedef struct {
-	int         key;					// so entities can reuse same entry
-	vec3_t      origin;
-	float       radius;
-	float       die;					// stop lighting after this time
-	float       decay;					// drop this each second
-	float       minlight;				// don't add when contributing less
-	float       color[3];
+	int			key;					// so entities can reuse same entry
+	vec3_t		origin;
+	float		radius;
+	float		die;					// stop lighting after this time
+	float		decay;					// drop this each second
+	float		minlight;				// don't add when contributing less
+	float		color[3];
 } dlight_t;
 
-void        R_PushDlights (void);
-void		R_MarkLights (dlight_t *light, int bit, model_t *model);
-void		R_MarkLightsNoVis (dlight_t *light, int bit, mnode_t *node);
-void        R_RenderDlights (void);
-void        R_AnimateLight (void);
-int         R_LightPoint (vec3_t p);
+void R_MarkLights (dlight_t *light, int bit, model_t *model);
+void R_MarkLightsNoVis (dlight_t *light, int bit, mnode_t *node);
+void R_AnimateLight (void);
+int R_LightPoint (vec3_t p);
+void R_RenderDlights (void);
+void R_PushDlights (void);
 
-void        R_InitParticles (void);
-void        R_ClearParticles (void);
-void        R_MoveParticles (void);
-void        R_DrawParticles (void);
-void        R_DrawWaterSurfaces (void);
-
-//
-// surface cache related
-//
-extern int  reinit_surfcache;			// if 1, surface cache is currently
-										// empty and
-extern qboolean r_cache_thrash;			// set if thrashing the surface cache
-
-int         D_SurfaceCacheForRes (int width, int height);
-void        D_FlushCaches (void);
-void        D_DeleteSurfaceCache (void);
-void        D_InitCaches (void *buffer, int size);
-void        R_SetVrect (vrect_t *pvrect, vrect_t *pvrectin, int lineadj);
+void R_InitParticles (void);
+void R_ClearParticles (void);
+void R_MoveParticles (void);
+void R_DrawParticles (void);
+void R_DrawWaterSurfaces (void);
 
 #endif // __RENDER_H
 
