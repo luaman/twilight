e76a639c30b6be940fcf575d3ac376713d9874b8
knghtbrd <knghtbrd@d7cf8633-e32d-0410-b094-e92efae38249>
2002-05-02 02:08:29 +0000

    This file got lost along the way...

---
+++ trunk/twilight/src/qw/cl_null.c
@@ -0,0 +1,43 @@
+/*
+	$RCSfile$
+
+	Copyright (C) 2002  Joseph Carter
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+
+	See the GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to:
+	
+		Free Software Foundation, Inc.
+		59 Temple Place - Suite 330
+		Boston, MA  02111-1307, USA
+*/
+static const char rcsid[] =
+	"$Id$";
+
+#include "twiconfig.h"
+
+/*
+ * Null client
+ *
+ * Basically, anything required to make the server compile without a client
+ * should go in here.  It's just a stub.
+ */
+	
+
+// Funtions that don't actually exist
+
+void
+Draw_Disc (void)
+{
+}
+
