eecd34e3a6c30e1b2dacdf10b63a45761f6908fb
mercury <mercury@d7cf8633-e32d-0410-b094-e92efae38249>
2002-05-14 05:09:21 +0000

    # USAGE: ./getglfunc.pl <path to gl.h> <path to glext.h> <function> # Simple enough.
    Finds the prototype for the function in question, prints it in the
    proper format for the dynamic opengl stuff.
    
    No, it is not clean, but I'm sick and this was a quick script Knghtbrd
    wanted.
    
    Requires the C::Scan perl module, available from CPAN and as a Debian
    package.

---
+++ trunk/twilight/src/tools/getglfunc.pl
@@ -0,0 +1,33 @@
+#!/bin/sh
+exec perl -w -x $0 ${1+"$@"} # -*- mode: perl; perl-indent-level: 2; -*-
+#!perl -w
+
+# USAGE: ./getglfunc.pl <path to gl.h> <path to glext.h> <function>
+# Simple enough.
+#
+# Copyright (C) 2002 Zephaniah E. Hull.
+
+use strict;
+use C::Scan;
+
+sub main {
+    my ($glh, $glexth, $func) = @_;
+    my ($c, $pfuncs, $pfunc, $arg, @args);
+
+    $c = C::Scan->new('filename' => $glexth, 'add_cppflags' => '-DGL_GLEXT_PROTOTYPES -DAPIENTRY="" -include ' . $glh);
+
+    $pfuncs = $c->get('parsed_fdecls');
+
+    foreach $pfunc (@{$pfuncs}) {
+	if ($pfunc->[1] eq $func) {
+	    foreach $arg (@{$pfunc->[2]}) {
+		push (@args, sprintf("%s %s", $arg->[0], $arg->[1]));
+	    }
+	    printf("OGL_NEED (%s, %s, (%s))\n",
+		    $pfunc->[0], $pfunc->[1], join(', ', @args));
+	    exit();
+	}
+    }
+}
+
+&main(@ARGV);
