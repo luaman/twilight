31f2e7d53035507e88d846e6a2d29a932f02f38b
eviltypeguy <eviltypeguy@d7cf8633-e32d-0410-b094-e92efae38249>
2002-08-29 06:08:09 +0000

    unify nq/keys.h qw/keys.h as include/keys.h

--- src/include/keys.h
+++ src/include/keys.h
@@ -10,7 +10,7 @@
 
 	This program is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
 	See the GNU General Public License for more details.
 
@@ -125,12 +125,12 @@ extern char chat_buffer[];
 extern unsigned chat_bufferlen;
 extern qboolean chat_team;
 
-void        Key_Event (int key, qboolean down);
-void        Key_Init_Cvars (void);
-void        Key_Init (void);
-void        Key_WriteBindings (FILE * f);
-void        Key_SetBinding (int keynum, char *binding);
-void        Key_ClearStates (void);
+void Key_Event (int key, qboolean down);
+void Key_Init_Cvars (void);
+void Key_Init (void);
+void Key_WriteBindings (FILE * f);
+void Key_SetBinding (int keynum, char *binding);
+void Key_ClearStates (void);
 
 #endif // __KEYS_H
 
