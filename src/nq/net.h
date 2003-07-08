1012ee06a28a7db2d96eadff2d9b7c6275e94933
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2003-07-08 12:02:27 +0000

    revert MAX_DATAGRAM to 1024 like stock quake used, to avoid crashing stock quake clients

--- src/nq/net.h
+++ src/nq/net.h
@@ -10,13 +10,13 @@
 
 	This program is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
 	See the GNU General Public License for more details.
 
 	You should have received a copy of the GNU General Public License
 	along with this program; if not, write to:
-	
+
 		Free Software Foundation, Inc.
 		59 Temple Place - Suite 330
 		Boston, MA  02111-1307, USA
@@ -30,7 +30,7 @@
 #include "common.h"
 
 #define	MAX_MSGLEN		8000
-#define	MAX_DATAGRAM	4096
+#define	MAX_DATAGRAM	1024
 
 struct qsockaddr {
 	short       sa_family;
